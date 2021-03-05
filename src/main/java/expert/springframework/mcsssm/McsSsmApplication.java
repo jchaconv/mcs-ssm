0563792367a6c51c2b8468ab2ff4564547b42bb1
jchaconv <jcaaron.chacon@outlook.com>
2021-03-05 15:56:32 -0500

    added initial files to the project

---
+++ src/main/java/expert/springframework/mcsssm/McsSsmApplication.java
@@ -0,0 +1,13 @@
+package expert.springframework.mcsssm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McsSsmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McsSsmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/expert/springframework/mcsssm/McsSsmApplicationTests.java
@@ -0,0 +1,13 @@
+package expert.springframework.mcsssm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McsSsmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
