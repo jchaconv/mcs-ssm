5f2ae6e00aa90eddcb01debc5ecc46cf9b1ec36f
jchaconv <jcaaron.chacon@outlook.com>
2021-03-05 17:06:51 -0500

    State Configuration

---
+++ src/main/java/expert/springframework/mcsssm/config/StateMachineConfig.java
@@ -0,0 +1,27 @@
+package expert.springframework.mcsssm.config;
+
+import expert.springframework.mcsssm.domain.PaymentEvent;
+import expert.springframework.mcsssm.domain.PaymentState;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.statemachine.config.EnableStateMachineFactory;
+import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
+import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
+
+import java.util.EnumSet;
+
+@Slf4j
+@EnableStateMachineFactory
+@Configuration
+public class StateMachineConfig extends StateMachineConfigurerAdapter<PaymentState, PaymentEvent> {
+
+    @Override
+    public void configure(StateMachineStateConfigurer<PaymentState, PaymentEvent> states) throws Exception {
+        states.withStates()
+                .initial(PaymentState.NEW)
+                .states(EnumSet.allOf(PaymentState.class))
+                .end(PaymentState.AUTH)
+                .end(PaymentState.PRE_AUTH_ERROR)
+                .end(PaymentState.AUTH_ERROR);
+    }
+}
