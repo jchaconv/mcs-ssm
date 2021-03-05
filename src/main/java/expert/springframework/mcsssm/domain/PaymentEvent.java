05a40b35464ec4025bfb3316cce7c5d867cf46b7
jchaconv <jcaaron.chacon@outlook.com>
2021-03-05 16:14:28 -0500

    State machine enumerations and dependency.

---
+++ src/main/java/expert/springframework/mcsssm/domain/PaymentEvent.java
@@ -0,0 +1,7 @@
+package expert.springframework.mcsssm.domain;
+
+public enum PaymentEvent {
+
+    PRE_AUTHORIZE, PRE_AUTH_APPROVED, PRE_AUTH_DECLINED, AUTHORIZE, AUTH_APPROVED, AUTH_DECLINED
+
+}
---
+++ src/main/java/expert/springframework/mcsssm/domain/PaymentState.java
@@ -0,0 +1,7 @@
+package expert.springframework.mcsssm.domain;
+
+public enum PaymentState {
+
+    NEW, PRE_AUTH, PRE_AUTH_ERROR, AUTH, AUTH_ERROR
+
+}
