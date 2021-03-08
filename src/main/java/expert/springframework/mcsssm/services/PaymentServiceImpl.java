948d97f284c16832b06b07fa8ced556f0ddf70cb
jchaconv <jcaaron.chacon@outlook.com>
2021-03-08 17:53:42 -0500

    State change interceptor

--- src/main/java/expert/springframework/mcsssm/services/PaymentServiceImpl.java
+++ src/main/java/expert/springframework/mcsssm/services/PaymentServiceImpl.java
@@ -16,10 +16,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class PaymentServiceImpl implements PaymentService {
 
-    private static final String PAYMENT_ID_HEADER = "payment_id";
+    public static final String PAYMENT_ID_HEADER = "payment_id";
 
     private final PaymentRepository paymentRepository;
     private final StateMachineFactory<PaymentState, PaymentEvent> stateMachineFactory;
+    private final PaymentStateChangeInterceptor paymentStateChangeInterceptor;
 
     @Override
     public Payment newPayment(Payment payment) {
@@ -71,6 +72,7 @@ public class PaymentServiceImpl implements PaymentService {
 
         sm.getStateMachineAccessor()
                 .doWithAllRegions(sma -> {
+                    sma.addStateMachineInterceptor(paymentStateChangeInterceptor);
                     sma.resetStateMachine(new DefaultStateMachineContext<>(payment.getState(), null, null, null));
                 });
 
